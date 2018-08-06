12a1abea60880c6db690b72d07421287c6091117
pethotelTU <kingdompets.tuvarna@gmail.com>
2018-08-06 08:19:18 +0300

    Minor changes on messege page

--- src/app/components/admin-components/messege-box/messege-box.component.ts
+++ src/app/components/admin-components/messege-box/messege-box.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { AdminService } from '../../../services/admin-services/admin.service';
+import { ContactViewModel } from '../../../models/view-models/public-models/contact-view-model';
+
 @Component({
-  selector: 'app-messege-box',
-  templateUrl: './messege-box.component.html',
-  styleUrls: ['./messege-box.component.scss']
+	selector: 'app-messege-box',
+	templateUrl: './messege-box.component.html',
+	styleUrls: ['./messege-box.component.scss']
 })
 export class MessegeBoxComponent implements OnInit {
+	messeges: Observable<ContactViewModel[]>;
+	constructor(private adminService: AdminService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.messeges = this.adminService.getReceivedMesseges().pipe();
+	}
 
 }
